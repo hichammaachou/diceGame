60584725457349049cf0799887cb129f240f2563
Hicham Maachou <34988698+hichammaachou@users.noreply.github.com>
2022-06-13 18:53:37 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var randomNum1= Math.floor(Math.random()*6)+1;
 var randomNum2= Math.floor(Math.random()*6)+1;
-document.querySelector("img").setAttribute("src", "dice"+randomNum1+".png");
-document.getElementsByClassName("img2")[0].setAttribute("src", "dice"+randomNum2+".png");
+document.querySelector("img").setAttribute("src", "Imgs/dice"+randomNum1+".png");
+document.getElementsByClassName("img2")[0].setAttribute("src", "Imgs/dice"+randomNum2+".png");
 if (randomNum1 === randomNum2){
     document.querySelector("h1").innerHTML ="Draw!";
 }else if(randomNum1<randomNum2){
